7b6136db5d8099cb0da0da4a3f17f8aee3f4f415
okkartal <okadirkartal@gmail.com>
2023-07-15 14:45:22 +0400

    returned empty user instead of null

--- src/Infrastructure/Handlers/BasicAuthenticationHandler.cs
+++ src/Infrastructure/Handlers/BasicAuthenticationHandler.cs
@@ -29,33 +29,43 @@ public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSc
         if (!Request.Headers.ContainsKey("Authorization"))
             return AuthenticateResult.Fail("Missing Authorization Header");
 
-        UserModel? user = null;
+        var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+
+        var credentials = authHeader.Parameter?.Split(':');
+        
+        if(credentials?.Length != 2)
+            return AuthenticateResult.Fail("Missing Authorization Header");
+
+
+        IEnumerable<UserModel> users;
         try
         {
-            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
-            var credentials = authHeader.Parameter?.Split(':');
-            var username = credentials?[0];
-            var password = credentials?[1];
-            if (username != null && password != null)
-                    user = await _userService.AuthenticateAsync(username, password);
-            if (user == null)
-                return await Task.FromResult(AuthenticateResult.Fail("Invalid Username or Password"));
+            
+            string username = credentials[0], password = credentials[1];
+            
+            users = await _userService.AuthenticateAsync(username, password);
+            
+            if (users.Any())
+            {
+                UserModel user = users.First();
+
+                var claims = new[]
+                    {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.UserData, user.Password)
+                };
+
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+
+                return await Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(principal, Scheme.Name)));
+
+            }
+            return await Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
         }
         catch
         {
             return await Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
         }
-
-
-        var claims = new[]
-        {
-            new Claim(ClaimTypes.Name, user.UserName),
-            new Claim(ClaimTypes.UserData, user.Password)
-        };
-
-        var identity = new ClaimsIdentity(claims, Scheme.Name);
-        var principal = new ClaimsPrincipal(identity);
-
-        return await Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(principal, Scheme.Name)));
     }
 }
